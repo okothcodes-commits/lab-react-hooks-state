a0f22547c73643a1f8b9990ad7bd359496bffea4
Eugene Oyier <okoth.codes@gmail.com>
2026-08-15 21:26:52 +0300

    fix: update Cart component to display items in the shopping cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Cart = () => {
+const Cart = ({ items }) => {
   return (
     <div>
       <h2>Shopping Cart</h2>
+
       <ul>
-        {/* TODO: Include items here in li tags with text 'ITEM.NAME is in your cart.' */}
+        {items.map((item) => (
+          <li key={item.id}>{item.name} is in your cart.</li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
